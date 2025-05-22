3daae7d7e88d07cd3af484fe550c7ceda2ce16ce
MatthewImmanuel <160089936+MatthewImmanuel@users.noreply.github.com>
2025-05-22 12:00:16 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://med-sync-6o63.vercel.app/', 
+        target: 'http://localhost:3000', 
         changeOrigin: true,
       }
     },
